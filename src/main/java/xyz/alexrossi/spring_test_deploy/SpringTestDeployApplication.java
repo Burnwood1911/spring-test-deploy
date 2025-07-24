6b5b4b485a23e1cbfb293a14f80c00fabc5563e9
Alex Rossi <alex.rossi@crdbbank.co.tz>
2025-07-24 04:22:18 +0300

    Add returns

---
+++ src/main/java/xyz/alexrossi/spring_test_deploy/SpringTestDeployApplication.java
@@ -0,0 +1,13 @@
+package xyz.alexrossi.spring_test_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestDeployApplication.class, args);
+	}
+
+}
